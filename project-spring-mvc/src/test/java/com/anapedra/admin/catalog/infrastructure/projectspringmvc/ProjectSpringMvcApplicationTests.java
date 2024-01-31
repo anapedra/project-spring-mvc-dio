28c1727893bc07a36cc81416ec3d6ad3d36af923
anapedra <anapedra.mil@gmail.com>
2024-01-31 07:27:47 -0300

    Project-spring-mvc created

---
+++ project-spring-mvc/src/test/java/com/anapedra/admin/catalog/infrastructure/projectspringmvc/ProjectSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anapedra.admin.catalog.infrastructure.projectspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
